7fc9d825e341e12e0b97e4c10769e2e28915e0dc
Jeffery Kwan How <jeffery0kwan0how@gmail.com>
2022-09-02 14:39:44 +1000

    Connected a database

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const env_variables = process.env;
 
 //------- Leave imports above ------
 
+
+//------- Host backend ----
+
 const app = express();
+
+// ---- Database -----
+const uri = `mongodb+srv://${env_variables.DB_username}:${env_variables.DB_password}@${env_variables.DB_domain}`;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology:true});
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// -------------------------
 const port = 8080;
 
 var mainRouter = require('./routes/main.routes');
